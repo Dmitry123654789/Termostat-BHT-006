11f8eece1a13639b632667b39d2f36fd916f0def
Dmitry <pokemon2708@yandex.ru>
2025-07-11 19:04:11 +0300

    Файлы перевода

---
+++ translations/ru-eng.ts
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_GB" sourcelanguage="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Termostat BHT</source>
+        <translation>Termostat BHT</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Опрос(мc)</source>
+        <translation>Survey(ms)</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Установить</source>
+        <translation>Install</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Подключиться</source>
+        <translation>Connect</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Отключиться</source>
+        <translation>Disconnect</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Блокировка клавиш</source>
+        <translation>Key lock</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Вкл/Откл</source>
+        <translation>On/Off</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>День недели</source>
+        <translation>Day of the week</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Минут</source>
+        <translation>Minutes</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Температура</source>
+        <translation>Temperature</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Уставка температуры (Ручной)</source>
+        <translation>Temperature setting (Manual)</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Авто/Ручной</source>
+        <translation>Auto/Manual</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Температура для недельного таймера</source>
+        <translation>Temperature for the weekly timer</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Питание нагревателя Вкл/Откл</source>
+        <translation>Heater power On/Off</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Час</source>
+        <translation>Hour</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Нагрев Вкл/Откл</source>
+        <translation>Heating On/Off</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Синхронизировать время и день недели с PC</source>
+        <translation>Synchronize time and day of the week with PC</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Инверсия 0/1</source>
+        <translation>Inversion 0/1</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Следующий</source>
+        <translation>Next</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Читать время и день недели </source>
+        <translation>Read the time and day of the week</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>1 Stop Bit</source>
+        <translation>1 Stop Bit</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>2 Stop Bits</source>
+        <translation>2 Stop Bits</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Modbus TCP/IP</source>
+        <translation>Modbus TCP/IP</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>COM порт</source>
+        <translation>COM port</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>ID</source>
+        <translation>ID</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>None Parity</source>
+        <translation>None Parity</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Odd Parity</source>
+        <translation>Odd Parity</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Even Parity</source>
+        <translation>Even Parity</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>8 Data bits</source>
+        <translation>8 Data bits</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>7 Data bits</source>
+        <translation>7 Data bits</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>9600 Baudrate</source>
+        <translation>9600 Baudrate</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>19200 Baudrate</source>
+        <translation>19200 Baudrate</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>38400 Baudrate</source>
+        <translation>38400 Baudrate</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>56000 Baudrate</source>
+        <translation>56000 Baudrate</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>115200 Baudrate</source>
+        <translation>115200 Baudrate</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>192.168.1.</source>
+        <translation>192.168.1.</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>IP</source>
+        <translation>IP</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Serial Port</source>
+        <translation>Serial Port</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Порт</source>
+        <translation>Port</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Опции</source>
+        <translation>Options</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Язык</source>
+        <translation>Language</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>Русcкий (ru)</source>
+        <translation>Русcкий (ru)</translation>
+    </message>
+    <message>
+        <location filename="qt/main_wind.ui" line="0"/>
+        <source>English (en)</source>
+        <translation>English (en)</translation>
+    </message>
+</context>
+</TS>
